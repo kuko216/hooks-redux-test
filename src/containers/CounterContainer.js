ba3adfb58c37deab7104de03f318827bc12ea767
Dotsung <dotsung22@gmail.com>
2019-10-16 12:23:25 +0900

    Update to useDispatch

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { useSelector, useActions } from "react-redux";
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
 import { increment, decrement } from "../modules/counter";
 
 const CounterContainer = () => {
   const counter = useSelector(state => state.counter, []);
 
-  const [onIncrease, onDecrease] = useActions([increment, decrement], []);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(
+    () => dispatch({ type: "counter/INCREMENT" }),
+    [dispatch]
+  );
+  const onDecrease = useCallback(
+    () => dispatch({ type: "counter/DECREMENT" }),
+    [dispatch]
+  );
 
   return (
     <Counter number={counter} onIncrease={onIncrease} onDecrease={onDecrease} />
