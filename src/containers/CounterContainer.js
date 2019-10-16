945e3c458fe2fcb49d9e6d96d396d38be8bef567
Dotsung <dotsung22@gmail.com>
2019-10-16 12:27:14 +0900

    Update type

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -7,14 +7,8 @@ const CounterContainer = () => {
   const counter = useSelector(state => state.counter, []);
 
   const dispatch = useDispatch();
-  const onIncrease = useCallback(
-    () => dispatch({ type: "counter/INCREMENT" }),
-    [dispatch]
-  );
-  const onDecrease = useCallback(
-    () => dispatch({ type: "counter/DECREMENT" }),
-    [dispatch]
-  );
+  const onIncrease = useCallback(() => dispatch(increment), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrement), [dispatch]);
 
   return (
     <Counter number={counter} onIncrease={onIncrease} onDecrease={onDecrease} />
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,8 +1,8 @@
 const INCREMENT = "counter/INCREMENT";
 const DECREMENT = "counter/DECREMENT";
 
-export const increment = () => ({ type: INCREMENT });
-export const decrement = () => ({ type: DECREMENT });
+export const increment = { type: INCREMENT };
+export const decrement = { type: DECREMENT };
 
 const initialState = 0;
 
